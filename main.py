0fc921c5b2a9ffd96fde3f629f4ab52621d63870
TimofeevAndrei <i.pika4u@gmail.com>
2023-02-18 22:16:04 +0700

    iterator coding

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class FlatIterator:
+
+    def __init__(self, list_of_list, n):
+        self.n = -1
+        self.list_of_list = list_of_list
+
+    def __iter__(self):
+        flat_list = []
+        for group in self.list_of_list:
+            flat_list.extend(group)
+        return self
+
+    def __next__(self):
+        self.n += 1
+        if self.n == len(flat_list):
+            raise StopIteration
+
+        return flat_list
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+        ]
+
+for item in FlatIterator(list_of_lists_1, 1):
+    print(item)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# list_inter = iter(list_of_lists_1)
+# in_list_iter = iter(list_inter.__next__())
+# print(in_list_iter.__next__())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# flat_list = FlatIterator(list_of_lists_1)
+#
+# for item in flat_list:
+#     print(item)
+
+# def test_1():
+#
+#     list_of_lists_1 = [
+#         ['a', 'b', 'c'],
+#         ['d', 'e', 'f', 'h', False],
+#         [1, 2, None]
+#     ]
+#
+#     for flat_iterator_item, check_item in zip(
+#             FlatIterator(list_of_lists_1),
+#             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+#     ):
+#
+#         assert flat_iterator_item == check_item
+#
+#     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+#
+#
+# if __name__ == '__main__':
+#     test_1()
